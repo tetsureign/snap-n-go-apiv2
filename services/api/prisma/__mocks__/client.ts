121a5220dc94133c843af765f621fd733750616b
Thu Nguyen <thuthien2001@gmail.com>
2026-01-19 15:37:08 +0700

    refactor: use the default node_modules prisma client for ease of deploying

--- services/api/prisma/__mocks__/client.ts
+++ services/api/prisma/__mocks__/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { beforeEach } from "vitest";
 import { mockDeep, mockReset } from "vitest-mock-extended";
 
